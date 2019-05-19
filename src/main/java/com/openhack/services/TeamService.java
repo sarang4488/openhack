8c22f27a322e832c46f5266ffb27cdb350ce3171
Shabari Girish Ganapathy <shabari8695@gmail.com>
2019-05-18 20:22:29 -0700

    hackathon registration validation

--- src/main/java/com/openhack/services/TeamService.java
+++ src/main/java/com/openhack/services/TeamService.java
@@ -14,9 +14,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 @Service
 public class TeamService {
@@ -55,7 +55,7 @@ public class TeamService {
                                           String member3_screenname,
                                           String member3_email,
                                           String member4_screenname,
-                                          String member4_email){
+                                          String member4_email) {
 
         long hackid = Optional.ofNullable(hid).orElse(-1L);
         Hackathon hackathon = hackathonDao.findItemById(hackid);
@@ -63,8 +63,29 @@ public class TeamService {
         User user2=null;
         User user3=null;
         User user4=null;
+
+        Date date1 = null;
+        Date date2 = null;
+
+        try {
+            date1 = new SimpleDateFormat("yyyy-MM-dd").parse(hackathon.getStart_date());
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+            String s2 = simpleDateFormat.format(new Date());
+            date2 = new SimpleDateFormat("yyy-MM-dd").parse(s2);
+
+            if (date2.compareTo(date1) > 0) {
+                return ResponseEntity.badRequest().body("Hackathon already started");
+            }
+        }catch (Exception e){
+            return ResponseEntity.badRequest().body("Cannot parse date exception");
+        }
+
+
         Team team = new Team();
         team.setHackathon(hackathon);
+
+        String owner = hackathon.getOwner().getName();
+        String [] judges = hackathon.getJudge_screenname().split("\\$");
 
         teamDao.createItem(team);
 
@@ -76,35 +97,62 @@ public class TeamService {
         teamMember1.setMember_id((int)user.getUid());
         teamMemberDao.createItem(teamMember1);
 
-        TeamMember teamMember2 = new TeamMember();
-        if(member2_screenname != null)
+        TeamMember teamMember2 = null ;
+        if(member2_screenname != null) {
+
+            if(member2_screenname.equals(owner))
+                return ResponseEntity.badRequest().body(owner+" is the owner. Owner cannot register for his hackathon");
+
+            if(Arrays.asList(judges).contains(member2_screenname))
+                return ResponseEntity.badRequest().body(member2_screenname+" is a judge. A judge cannot be registerd fot the same hackathon");
+
             user2 = userDao.findByScreenname(member4_screenname);
+            teamMember2 = new TeamMember();
+        }
 
-        if(user2 != null)
-            teamMember2.setMember_id((int)user2.getUid());
+        if(user2 != null ) {
+            teamMember2.setMember_id((int) user2.getUid());
+            teamMember2.setTeam(team);
+            teamMemberDao.createItem(teamMember2);
+        }
 
-        teamMember2.setTeam(team);
-        teamMemberDao.createItem(teamMember2);
+        TeamMember teamMember3 = null;
+        if(member3_screenname != null) {
 
-        TeamMember teamMember3 = new TeamMember();
-        if(member3_screenname != null)
+            if(member3_screenname.equals(owner))
+                return ResponseEntity.badRequest().body(owner+" is the owner. Owner cannot register for his hackathon");
+
+            if(Arrays.asList(judges).contains(member3_screenname))
+                return ResponseEntity.badRequest().body(member3_screenname+" is a judge. A judge cannot be registerd fot the same hackathon");
+
             user3 = userDao.findByScreenname(member4_screenname);
+            teamMember3 = new TeamMember();
+        }
 
-        if(user3 != null)
-            teamMember3.setMember_id((int)user3.getUid());
+        if(user3 != null) {
+            teamMember3.setMember_id((int) user3.getUid());
+            teamMember3.setTeam(team);
+            teamMemberDao.createItem(teamMember3);
+        }
 
-        teamMember3.setTeam(team);
-        teamMemberDao.createItem(teamMember3);
+        TeamMember teamMember4 = null;
+        if(member4_screenname != null) {
 
-        TeamMember teamMember4 = new TeamMember();
-        if(member4_screenname != null)
+            if(member4_screenname.equals(owner))
+                return ResponseEntity.badRequest().body(owner+" is the owner. Owner cannot register for his hackathon");
+
+            if(Arrays.asList(judges).contains(member4_screenname))
+                return ResponseEntity.badRequest().body(member4_screenname+" is a judge. A judge cannot be registerd fot the same hackathon");
+
             user4 = userDao.findByScreenname(member4_screenname);
+            teamMember4 = new TeamMember();
+        }
 
-        if(user4 != null)
-            teamMember4.setMember_id((int)user4.getUid());
-
-        teamMember4.setTeam(team);
-        teamMemberDao.createItem(teamMember4);
+        if(user4 != null) {
+            teamMember4.setMember_id((int) user4.getUid());
+            teamMember4.setTeam(team);
+            teamMemberDao.createItem(teamMember4);
+        }
 
         Team updated_team = teamDao.findById(team.getTid());
 
