788e2eeeedb800a6407d3bbe4f305c5a89a40c20
Jasnoor Brar <jasnoor.brar@sjsu.edu>
2019-05-20 00:49:19 -0700

    Junit test case for TeamService.java

---
+++ src/test/java/com/openhack/services/TeamServiceTest.java
@@ -0,0 +1,14 @@
+package com.openhack.services;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TeamServiceTest {
+
+	@Test
+	void test() {
+		TeamService obj=new TeamService();
+	}
+
+}
