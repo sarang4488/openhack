95c2780e61d36e7b24d016f13eb3eef7d4351c32
Jasnoor Brar <jasnoor.brar@sjsu.edu>
2019-05-20 00:48:56 -0700

    Junit test case for UserService.java

---
+++ src/test/java/com/openhack/services/UserServiceTest.java
@@ -0,0 +1,14 @@
+package com.openhack.services;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class UserServiceTest {
+
+	@Test
+	void test() {
+		UserService obj=new UserService();
+	}
+
+}
